3dfb7ca803f934aa809263ef29d3779210cf7062
Marco Coccato <coccato.m@gmail.com>
2019-12-22 22:00:45 +0100

    Added random seed and offsets

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,22 +6,51 @@ namespace Ocrambana.LandmassGeneration
 {
     public class MapGenerator : MonoBehaviour
     {
-        public int  
+        public int
             mapWidth = 10,
-            mapHeight = 10,
-            octaves;
-        public float   
-            noiseScale,
-            persistance = 0.5f,
-            lacunarity = 2f;
+            mapHeight = 10;
+        public float noiseScale;
+
+        public int octaves;
+        [Range(0f,1f)]
+        public float persistance = 0.5f;
+        public float lacunarity = 2f;
+
+        public int seed;
+        public Vector2 offset;
+
         public bool autoUpdate;
 
         public void GenerateMap()
         {
-            float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, octaves, persistance, lacunarity);
+            float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
             MapDisplay display = FindObjectOfType<MapDisplay>();
             display.DrawNoiseMap(noiseMap);
+        }
+
+        private void OnValidate()
+        {
+            if(mapWidth < 1)
+            {
+                mapWidth = 1;
+            }
+
+            if(mapHeight < 1)
+            {
+                mapHeight = 1;
+            }
+
+            if(lacunarity < 1)
+            {
+                lacunarity = 1;
+            }
+
+            if(octaves < 0)
+            {
+                octaves = 0;
+            }
+
         }
     }
 }
--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -5,13 +5,28 @@ namespace Ocrambana.LandmassGeneration
 {
     internal static class Noise
     {
-        internal static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int octaves, float persistance, float lacunarity)
+        internal static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
         {
             float[,] noiseMap = new float[mapWidth, mapHeight];
+
+            System.Random prng = new System.Random(seed);
+            Vector2[] octavesOffset = new Vector2[octaves]; 
+
+            for(int i = 0; i < octaves; i++)
+            {
+                float offsetX = prng.Next(-100000, 100000) + offset.x;
+                float offsetY = prng.Next(-100000, 100000) + offset.y;
+                octavesOffset[i] = new Vector2(offsetX, offsetY);
+            }
+
             float   
                 actualScale = scale,
                 maxNoiseHeight = float.MinValue,
                 minNoiseHeight = float.MaxValue;
+
+            float
+                halfWIdth = mapWidth / 2f,
+                halfHeight = mapHeight / 2f;
 
             if(scale <= 0)
             {
@@ -30,8 +45,8 @@ namespace Ocrambana.LandmassGeneration
                     for(int o = 0; o < octaves; o++)
                     {
                         float   
-                            sampleX = i / actualScale * frequency,
-                            sampleY = j / actualScale * frequency;
+                            sampleX = (i - halfWIdth) / actualScale * frequency + octavesOffset[o].x,
+                            sampleY = (j - halfHeight) / actualScale * frequency + octavesOffset[o].y;
 
                         float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
                         noiseHeight += perlinValue * amplitude;
